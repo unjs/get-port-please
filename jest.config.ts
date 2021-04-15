e16b3b16a6956bc1e070214ab9435b0620067410
Tom Mrazauskas <tom@mrazauskas.de>
2021-04-15 07:38:25 +0300

    test: add coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   preset: 'ts-jest',
   testEnvironment: 'node'
 }
